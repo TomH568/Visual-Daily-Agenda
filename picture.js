290db8289c45c7b9685442bda092962a3107d999
DM-fin <denis.miliushchenko@edu.karelia.fi>
2026-09-17 13:58:34 +0300

    class returns the path the the picture (by keyword)

--- picture.js
+++ picture.js
@@ -8,6 +8,7 @@ class Picture {
     }
     show() {
         const files = fs.readdirSync(".");
+        
         for (const file of files) {
             const ext = path.extname(file).toLowerCase(); 
             const fileName = path.basename(file).toLowerCase();
